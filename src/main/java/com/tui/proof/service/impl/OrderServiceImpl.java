b513105eb48e7bc9dc71107bb4374edbe504f275
Luca Saletta <luca.salettasillero@colaborador.viajeseci.es>
2021-08-01 11:04:58 +0200

    Added search orders logic and handler for ConstraintViolationException

--- src/main/java/com/tui/proof/service/impl/OrderServiceImpl.java
+++ src/main/java/com/tui/proof/service/impl/OrderServiceImpl.java
@@ -1,12 +1,15 @@
 package com.tui.proof.service.impl;
 
+import com.tui.proof.domain.entity.OrderEntity;
 import com.tui.proof.domain.repository.OrderRepository;
 import com.tui.proof.exception.TUIMMException;
+import com.tui.proof.mapper.OrderMapper;
 import com.tui.proof.model.Order;
 import com.tui.proof.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 
@@ -17,9 +20,29 @@ public class OrderServiceImpl implements OrderService {
 
     private final OrderRepository orderRepository;
 
-
+    /**
+     * Search orders by customer data, allowing partial searches.
+     * @param page
+     * @param size
+     * @param name
+     * @param lastName
+     * @return paged orders
+     * @throws TUIMMException
+     */
     @Override
-    public Page<Order> getOrders(int page, int size, String name, String lastName) throws TUIMMException {
-        return null;
+    public Page<Order> getOrders(final int page, final int size, final String name, final String lastName)
+            throws TUIMMException {
+        try {
+            Page<OrderEntity> byClientInfo =
+                    orderRepository.getByClientInfo(name, lastName, PageRequest.of(page, size));
+            if (byClientInfo.getTotalElements() != 0L) {
+                return byClientInfo.map(item -> OrderMapper.INSTANCE.toDto(item));
+            }
+        } catch (Exception e) {
+            log.error("Error in order search: {}", e);
+            throw new TUIMMException("Error in order search", e);
+        }
+
+        throw new TUIMMException("No orders content");
     }
 }
--- src/main/java/com/tui/proof/ws/handler/ControllerExceptionHandler.java
+++ src/main/java/com/tui/proof/ws/handler/ControllerExceptionHandler.java
@@ -9,19 +9,36 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
+import javax.validation.ConstraintViolationException;
+
 @RestControllerAdvice
 public class ControllerExceptionHandler {
 
     /**
      * Handler {@link TUIMMException}.
+     *
      * @param ex
      * @param request
      * @return response with NO_CONTENT httpStatus
      */
     @ExceptionHandler(TUIMMException.class)
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
-    public ErrorMessage handler(final TUIMMException ex, final WebRequest request) {
+    public ErrorMessage handlerTUIMM(final TUIMMException ex, final WebRequest request) {
         ErrorMessage message = new ErrorMessage(ex.getErrorDescription());
+        return message;
+    }
+
+    /**
+     * Handler {@link ConstraintViolationException}.
+     *
+     * @param ex
+     * @param request
+     * @return response with BAD_REQUEST httpStatus
+     */
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public ErrorMessage handlerCV(final ConstraintViolationException ex, final WebRequest request) {
+        ErrorMessage message = new ErrorMessage(ex.getMessage());
         return message;
     }
 }
