ea6967c63529e4bcd4e7bcac545df12dd23245b8
Luca Saletta <luca.salettasillero@colaborador.viajeseci.es>
2021-08-05 14:10:48 +0200

    Fixed controller handler

--- src/main/java/com/tui/proof/ws/handler/ControllerExceptionHandler.java
+++ src/main/java/com/tui/proof/ws/handler/ControllerExceptionHandler.java
@@ -4,20 +4,23 @@ package com.tui.proof.ws.handler;
 import com.tui.proof.exception.TUIMMException;
 import org.apache.commons.lang3.StringUtils;
 import org.springdoc.api.ErrorMessage;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.validation.ConstraintViolationException;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RestControllerAdvice
-public class ControllerExceptionHandler {
+public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
 
     /**
      * Handler {@link TUIMMException}.
@@ -27,7 +30,7 @@ public class ControllerExceptionHandler {
      * @return response with NO_CONTENT httpStatus
      */
     @ExceptionHandler(TUIMMException.class)
-    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
     public ErrorMessage handlerTUIMM(final TUIMMException ex, final WebRequest request) {
         ErrorMessage message = new ErrorMessage(ex.getErrorDescription());
         return message;
@@ -55,11 +58,11 @@ public class ControllerExceptionHandler {
      * @param request
      * @return response with BAD_REQUEST httpStatus
      */
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
-    public ErrorMessage handlerErrorValidations(final MethodArgumentNotValidException ex,
-                                                final WebRequest request) {
 
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers, HttpStatus status,
+                                                                  WebRequest request) {
         String fieldErrors = StringUtils.EMPTY;
         if (ex.hasErrors()) {
             List<FieldError> fieldErrorList = ex.getFieldErrors();
@@ -67,7 +70,6 @@ public class ControllerExceptionHandler {
                     .map(fieldError -> fieldError.getField() + ": " + fieldError.getDefaultMessage())
                     .collect(Collectors.joining(","));
         }
-        return new ErrorMessage(fieldErrors);
-
+        return new ResponseEntity<>(new ErrorMessage(fieldErrors), HttpStatus.BAD_REQUEST);
     }
 }
